80e64a3c5d87cdfe7c7a5afd33a5e44e6bc2b8a3
galen <galenlong@kuainiugroup.com>
2020-04-03 10:19:24 +0800

    dir

--- dongfangcaifu_zijin/run.sh
+++ dongfangcaifu_zijin/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cd /root/QuantScrapy/dongfangcaifu_zijin/ && /usr/local/bin/scrapy crawl zijin -o run.csv
+cd /root/QuantScrapy/dongfangcaifu_zijin/dongfangcaifu_zijin/spiders && /usr/local/bin/scrapy crawl zijin -o run.csv
